a906f3527d879df4b5ef33c162137e7a068732a6
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-01 19:41:46 +0100

    Start

---
+++ src/main/java/it/unimi/di/sweng/esame/model/Model.java
@@ -0,0 +1,29 @@
+package it.unimi.di.sweng.esame.model;
+
+
+import it.unimi.di.sweng.esame.Main;
+
+
+import java.io.InputStream;
+
+import java.util.*;
+
+public class Model {
+
+  //TODO completare la classe
+
+  public void readFile() {
+    InputStream is = Main.class.getResourceAsStream("/FinalistNations");
+    assert is != null;
+    Scanner s = new Scanner(is);
+
+    while (s.hasNextLine()) {
+      String linea = s.nextLine();
+      String[] el = linea.split(";");
+      String name = el[0];
+      String cod = el[1];
+
+      System.out.printf("cod: [%s] name: [%s]\n", cod, name);
+    }
+  }
+}
