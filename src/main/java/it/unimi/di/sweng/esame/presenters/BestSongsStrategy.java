4af9a97f59fdd589edb74641962da949cdfe597b
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-04 14:55:06 +0100

    VERDE: DisplayViewStrategyTest ora funziona

--- src/main/java/it/unimi/di/sweng/esame/presenters/BestSongsStrategy.java
+++ src/main/java/it/unimi/di/sweng/esame/presenters/BestSongsStrategy.java
@@ -2,12 +2,18 @@ package it.unimi.di.sweng.esame.presenters;
 
 import it.unimi.di.sweng.esame.model.Punteggio;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class BestSongsStrategy implements DisplayViewStrategy {
 
     @Override
     public void sortNazioni(List<Punteggio> punti) {
+        punti.sort((o1, o2) -> {
+            int res = Integer.compare(o1.punti(), o2.punti());
+            if(res == 0) return o1.nazione().nome().compareTo(o2.nazione().nome());
+            return res;
+        });
 
     }
 }
