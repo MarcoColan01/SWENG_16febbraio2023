54da14f7f917b1f281f1ff526d37274aed9ce030
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-04 10:20:36 +0100

    VERDE: testVotoLength ora funziona

--- src/main/java/it/unimi/di/sweng/esame/presenters/NextNationPresenter.java
+++ src/main/java/it/unimi/di/sweng/esame/presenters/NextNationPresenter.java
@@ -1,13 +1,21 @@
 package it.unimi.di.sweng.esame.presenters;
 
 import it.unimi.di.sweng.esame.views.NextNationView;
+import org.jetbrains.annotations.NotNull;
 
 public class NextNationPresenter implements Presenter{
-    public NextNationPresenter(NextNationView view) {
+    private final @NotNull NextNationView view;
+    public NextNationPresenter(@NotNull NextNationView view) {
+        this.view = view;
+        view.addHandlers(this);
     }
 
     @Override
-    public void action(String text1, String text2) {
-
+    public void action(String comando, String voto) {
+        String[] voti = voto.split(" ");
+        if(voti.length != 5){
+            view.showError("Invalid number of votes");
+            return;
+        }
     }
 }
