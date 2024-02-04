060f3fd0d0d384d02cc302f991b8791a58ff81dc
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-04 12:14:14 +0100

    Implementata la parte fredda del pattern observer

--- src/main/java/it/unimi/di/sweng/esame/model/Model.java
+++ src/main/java/it/unimi/di/sweng/esame/model/Model.java
@@ -2,6 +2,8 @@ package it.unimi.di.sweng.esame.model;
 
 
 import it.unimi.di.sweng.esame.Main;
+import it.unimi.di.sweng.esame.Observable;
+import it.unimi.di.sweng.esame.Observer;
 import it.unimi.di.sweng.esame.presenters.Nazione;
 import it.unimi.di.sweng.esame.presenters.Voto;
 import org.jetbrains.annotations.NotNull;
@@ -11,44 +13,58 @@ import java.io.InputStream;
 
 import java.util.*;
 
-public class Model {
+public class Model implements Observable<List<Voto>> {
 
-  private final @NotNull List<Nazione> nazioni = new ArrayList<>();
-  private final @NotNull Map<Nazione, Voto> voti = new HashMap<>();
-  private final @NotNull Map<Nazione, Integer> punti = new HashMap<>();
+    private final @NotNull List<Nazione> nazioni = new ArrayList<>();
+    private final @NotNull Map<Nazione, Voto> voti = new HashMap<>();
+    private final @NotNull Map<Nazione, Integer> punti = new HashMap<>();
 
-  public void readFile() {
-    InputStream is = Main.class.getResourceAsStream("/FinalistNations");
-    assert is != null;
-    Scanner s = new Scanner(is);
+    private final @NotNull List<Observer<List<Voto>>> observers = new ArrayList<>();
 
-    while (s.hasNextLine()) {
-      String linea = s.nextLine();
-      String[] el = linea.split(";");
-      String name = el[0];
-      String cod = el[1];
-      nazioni.add(Nazione.valueOf(cod));
+    public void readFile() {
+        InputStream is = Main.class.getResourceAsStream("/FinalistNations");
+        assert is != null;
+        Scanner s = new Scanner(is);
+
+        while (s.hasNextLine()) {
+            String linea = s.nextLine();
+            String[] el = linea.split(";");
+            String name = el[0];
+            String cod = el[1];
+            nazioni.add(Nazione.valueOf(cod));
+        }
     }
-  }
 
-  public List<Nazione> getNazioni() {
-    return new ArrayList<>(nazioni);
-  }
+    public List<Nazione> getNazioni() {
+        return new ArrayList<>(nazioni);
+    }
 
-  public void addVoto(@NotNull Voto voto) {
-   if(!voti.containsKey(voto.nazione())) voti.put(voto.nazione(), voto);
-   int points = 5;
-   for(Nazione nazione: voto.votate()){
-     punti.put(nazione, points);
-     points--;
-   }
-  }
+    public void addVoto(@NotNull Voto voto) {
+        if (!voti.containsKey(voto.nazione())) voti.put(voto.nazione(), voto);
+        int points = 5;
+        for (Nazione nazione : voto.votate()) {
+            punti.put(nazione, points);
+            points--;
+        }
+    }
 
-  public List<Voto> getVoti() {
-    return new ArrayList<>(voti.values());
-  }
+    public List<Voto> getVoti() {
+        return new ArrayList<>(voti.values());
+    }
 
-  public int getPunti(@NotNull String nazione) {
-    return punti.get(Nazione.valueOf(nazione));
-  }
+    public int getPunti(@NotNull String nazione) {
+        return punti.get(Nazione.valueOf(nazione));
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer<List<Voto>> observer : observers) {
+            observer.update(this);
+        }
+    }
+
+    @Override
+    public void addObserver(@NotNull Observer<List<Voto>> observer) {
+        observers.add(observer);
+    }
 }
